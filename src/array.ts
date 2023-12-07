d1c75f6b2369753dc10e81920435ad98d0190589
Philip Leung <>
2023-12-07 19:04:27 +0000

    array type code added

---
+++ src/array.ts
@@ -0,0 +1,12 @@
+const arr:number[] = [1,2,3]
+arr.push(4)
+console.log(arr)
+
+const arrString:string[] = ['a','b','c']
+console.log(arrString)
+
+const anyArr : any[] =['a',true, 1]
+console.log(anyArr)
+
+const specificArr:[number,string,boolean,null]=[1, 'hello', true, null]
+console.log(specificArr)
---
+++ src/basic.ts
@@ -0,0 +1,16 @@
+let a:number = 1
+
+let b:string = 'hello'
+
+a = 'morning'
+
+let c:boolean
+let d:null
+let e:undefined
+
+// Type inference
+let y = 2
+
+// const
+const u = 3
+const w = 'string'
