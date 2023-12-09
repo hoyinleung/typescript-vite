c34940a8d7e928bc4a41d75cc7ebc8c298b925b7
Philip Leung <>
2023-12-09 16:56:21 +0000

    object lesson code

---
+++ src/object.ts
@@ -0,0 +1,19 @@
+const memberInfo:{
+    name:string,
+    age:number,
+    address?:string
+} = {
+    name : 'philip',
+    age : 12,
+    address:'Hong Kong'
+}
+
+const memberInfo2:{
+    name:string,
+    age:number,
+    address?:string
+} = {
+    name : 'Patrick',
+    age : 13,
+    address:'Hong Kong'
+}
